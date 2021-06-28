0d499a493995ecbfd8a8a9f8446dc28f1327c9bd
NatStopa <nat.stopa.nat@gmail.com>
2021-06-28 09:38:04 +0200

    Added footer with GitHub link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        Coded by NatStopa, open-sourced on{" "}
+        <a
+          href="https://github.com/NatStopa/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
