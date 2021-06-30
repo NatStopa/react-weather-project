1af6ddfe8536f2928f7f9b296c7cfa097f9b80dd
NatStopa <nat.stopa.nat@gmail.com>
2021-06-30 09:06:49 +0200

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Coded by NatStopa, open-sourced on{" "}
-        <a
-          href="https://github.com/NatStopa/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Coded by NatStopa, open-sourced on{" "}
+          <a
+            href="https://github.com/NatStopa/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
