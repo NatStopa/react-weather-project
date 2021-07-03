af1321cb86bd3ecc268623b69125c1f707de9e4c
NatStopa <nat.stopa.nat@gmail.com>
2021-07-03 10:55:19 +0200

    Added icons and max min temperature

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Weather.css";
+import humidityicon from "./humidity.png";
+import weathericon from "./sun.png";
+import windicon from "./windsock.png";
 
 export default function Weather() {
   return (
@@ -19,21 +22,53 @@ export default function Weather() {
         </div>
       </form>
       <p className="current-date">Saturday 15:00</p>
+      <hr />
       <div className="current-weather">
-        <h1 className="current-city">Krakow</h1>
+        <h1 className="current-city">Los Angeles</h1>
         <p className="current-temp">20°</p>
-        <ul className="current-weather-props">
+      </div>
+      <hr />
+      <div className="row">
+        <ul className="current-weather-props col-7">
           <li className="current-description row">
-            <div className="col-1">☀️</div>
-            <div className="col-11">Clear sky</div>
+            <div className="col-3">
+              <img
+                src={weathericon}
+                alt="weather icon"
+                className="description-icons"
+              />
+            </div>
+            <div className="col-9">Clear sky</div>
           </li>
           <li className="humidity row">
-            <div className="col-1">☔︎</div>
-            <div className="col-11">60%</div>
+            <div className="col-3">
+              <img
+                src={humidityicon}
+                alt="humidity icon"
+                className="description-icons"
+              />
+            </div>
+            <div className="col-9">60%</div>
           </li>
           <li className="wind row">
-            <div className="col-1">☴</div>
-            <div className="col-11">3km/h</div>
+            <div className="col-3">
+              <img
+                src={windicon}
+                alt="wind icon"
+                className="description-icons"
+              />
+            </div>
+            <div className="col-9">3km/h</div>
+          </li>
+        </ul>
+        <ul className="current-weather-props col-4">
+          <li className="row">
+            <strong className="col-4">Max</strong>
+            <div className="col-8">25°</div>
+          </li>
+          <li className="row">
+            <strong className="col-4">Min</strong>
+            <div className="col-8">19°</div>
           </li>
         </ul>
       </div>
