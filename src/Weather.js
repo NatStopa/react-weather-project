7d8b4045ba5098dd81b0382128df5b0022f00cda
NatStopa <nat.stopa.nat@gmail.com>
2021-07-18 09:26:49 +0200

    Added search engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
-import FormatDate from "./FormatDate";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
-import humidityicon from "./humidity.png";
-import weathericon from "./sun.png";
-import windicon from "./windsock.png";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
     setWeatherData({
       date: new Date(response.data.dt * 1000),
@@ -24,16 +22,32 @@ export default function Weather(props) {
     });
   }
 
+  function search() {
+    const apiKey = "02ae2bfab4b783181c5ec4a0935ec345";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
                 type="search"
                 placeholder="Enter a city..."
                 className="form-control city-input"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
@@ -45,67 +59,11 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <p className="current-date">
-          <FormatDate date={weatherData.date} />
-        </p>
-        <hr />
-        <div className="current-weather">
-          <h1 className="current-city">{weatherData.city}</h1>
-          <p className="current-temp">{Math.round(weatherData.temp)}°C</p>
-        </div>
-        <hr />
-        <div className="row">
-          <ul className="current-weather-props col-6">
-            <li className="current-description row">
-              <div className="col-3">
-                <img
-                  src={weathericon}
-                  alt="weather icon"
-                  className="description-icons"
-                />
-              </div>
-              <div className="col-9 text-capitalize">
-                {weatherData.description}
-              </div>
-            </li>
-            <li className="humidity row">
-              <div className="col-3">
-                <img
-                  src={humidityicon}
-                  alt="humidity icon"
-                  className="description-icons"
-                />
-              </div>
-              <div className="col-9">{weatherData.humidity}%</div>
-            </li>
-            <li className="wind row">
-              <div className="col-3">
-                <img
-                  src={windicon}
-                  alt="wind icon"
-                  className="description-icons"
-                />
-              </div>
-              <div className="col-9">{Math.round(weatherData.wind)}km/h</div>
-            </li>
-          </ul>
-          <ul className="current-weather-props col-5">
-            <li className="row">
-              <strong className="col-4">Max</strong>
-              <div className="col-8">{Math.round(weatherData.max)}°</div>
-            </li>
-            <li className="row">
-              <strong className="col-4">Min</strong>
-              <div className="col-8">{Math.round(weatherData.min)}°</div>
-            </li>
-          </ul>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
-    const apiKey = "02ae2bfab4b783181c5ec4a0935ec345";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
     return (
       <div className="loader">
         <Loader
---
+++ src/WeatherInfo.js
@@ -0,0 +1,67 @@
+import React from "react";
+import FormatDate from "./FormatDate";
+import humidityicon from "./humidity.png";
+import weathericon from "./sun.png";
+import windicon from "./windsock.png";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <p className="current-date">
+        <FormatDate date={props.data.date} />
+      </p>
+      <hr />
+      <div className="current-weather">
+        <h1 className="current-city">{props.data.city}</h1>
+        <p className="current-temp">{Math.round(props.data.temp)}°C</p>
+      </div>
+      <hr />
+      <div className="row">
+        <ul className="current-weather-props col-7">
+          <li className="current-description row">
+            <div className="col-3 icons">
+              <img
+                src={weathericon}
+                alt="weather icon"
+                className="description-icons"
+              />
+            </div>
+            <div className="col-9 text-capitalize">
+              {props.data.description}
+            </div>
+          </li>
+          <li className="humidity row">
+            <div className="col-3 icons">
+              <img
+                src={humidityicon}
+                alt="humidity icon"
+                className="description-icons"
+              />
+            </div>
+            <div className="col-9">{props.data.humidity}%</div>
+          </li>
+          <li className="wind row">
+            <div className="col-3 icons">
+              <img
+                src={windicon}
+                alt="wind icon"
+                className="description-icons"
+              />
+            </div>
+            <div className="col-9">{Math.round(props.data.wind)}km/h</div>
+          </li>
+        </ul>
+        <ul className="current-weather-props col-4">
+          <li className="row">
+            <strong className="col-4">Max</strong>
+            <div className="col-8">{Math.round(props.data.max)}°</div>
+          </li>
+          <li className="row">
+            <strong className="col-4">Min</strong>
+            <div className="col-8">{Math.round(props.data.min)}°</div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
